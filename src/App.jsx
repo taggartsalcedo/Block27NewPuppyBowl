7091b0a62ba05be842f40a233c92d0690622640f
Taggart <t.salcedo@hotmail.com>
2023-11-20 16:36:38 -0500

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import PuppyList from './components/PuppyList'; 
 import './App.css'
 
 function App() {
@@ -5,7 +7,7 @@ function App() {
 
   return (
     <>
-
+    <PuppyList />
     </>
   );
 }
